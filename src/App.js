d713d7dcd37c978eff8cdb8a490b72d8439e32d5
KaraKanetis <kkanetis@hawaii.edu>
2025-07-09 22:01:15 -1000

    Task 2

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Container } from 'react-bootstrap';
+
+function App() {
+  return (
+    <main>
+      <Container className="text-center">
+        <h1>Honolulu Botanical Gardens</h1>
+        <p>Explore the beauty of Hawai‘i’s plants and gardens.</p>
+      </Container>
+    </main>
+  );
+}
+
+export default App;
